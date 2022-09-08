3416fd6c0cadc3b2aed3769e653bda52a5566fe5
vinayakgarole <vinayakgarole10@gmail.com>
2022-09-08 22:57:27 +0530

    added: Calculate the Number of Digit

---
+++ src/NumberOfDigit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NumberOfDigit {
+    public static void main(String[] args) {
+        int n,count=0;
+        System.out.println("Enter any Number ");
+        Scanner r=new Scanner(System.in);
+        n=r.nextInt();
+        while(n>0) {
+            n=n/10;
+            count++;
+        }
+        System.out.println("Sum of Digit Number "+count);
+    }
+}
